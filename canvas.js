14d53a637d72223df4141d902271633bd67269eb
imahdi2000 <imahdi@stuy.edu>
2018-02-05 23:48:02 -0500

    lines

--- canvas.js
+++ canvas.js
@@ -4,10 +4,14 @@ var context = c.getContext('2d');
 var color = "green";
 var r;
 //var button = clearC.addEventListener('click', clear);
+var saveoldposX;
+var saveoldposY;
 
 var clearC = function(){
   //console.log("call");
   context.clearRect(0,0,500,500);
+  saveoldposY = null;
+  saveoldposX = null;
   //or (0,0,context.width,context.height)
 };
 
@@ -33,14 +37,20 @@ var drawRect = function(e){
   context.beginPath();
   //console.log(e.clientX);
   //console.log(e.clientY);
-  context.fillRect(e.clientX-r.left,e.clientY-r.top,20,20);
+  context.fillRect(e.offsetX,e.offsetY,20,20);
   context.fill();
   context.stroke();
 };
 
 var drawCircle = function(e) {
   context.beginPath();
-  context.arc((e.clientX - r.left),(e.clientY - r.top),10,0,2 * Math.PI);
+  context.arc(e.offsetX,e.offsetY,10,0,2 * Math.PI);
+  if (saveoldposX != null){
+    context.moveTo(saveoldposX, saveoldposY);
+  	context.lineTo(e.offsetX, e.offsetY);
+  }
+  saveoldposX = e.offsetX;
+  saveoldposY = e.offsetY;
   context.fill();
   context.stroke();
 };
